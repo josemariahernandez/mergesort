dd8f2bfe2ffbecb463e4598b27925269ce48ce21
Jose Maria <josemaria.hernandez@medianet.es>
2015-06-11 17:24:53 +0200

    Cambio de codigo

--- mergesort.rb
+++ mergesort.rb
@@ -1,41 +1,43 @@
+#
 class Mergesort
-	def merge_sort(array)
-		if array.count <= 1
-			return array
-		end
+  def merge_sort(array)
+    return array if array.count <= 1
 
-		mitad1 = merge_sort(array.slice(0, array.count/2))
-		mitad2 = merge_sort(array.slice(array.count/2, array.count-array.count/2))
+    mitad_array = array.count / 2
+    mitad1 = merge_sort(array.slice(0, mitad_array))
+    mitad2 = merge_sort(array.slice(mitad_array, array.count - mitad_array))
+    array_temp = []
+    cont_temp = 0
+    cont1 = 0
+    cont2 = 0
 
-		array_temp = []
-		cont_temp = 0 
-		cont1 = 0
-		cont2 = 0
+    while cont1 < mitad1.count && cont2 < mitad2.count
+      if mitad1[cont1] <= mitad2[cont2]
+        array_temp[cont_temp] = mitad1[cont1]
+        cont_temp += 1
+        cont1 += 1
+      else
+        array_temp[cont_temp] = mitad2[cont2]
+        cont_temp += 1
+        cont2 += 1
+      end
+    end
 
-		while cont1 < mitad1.count && cont2 < mitad2.count
-			if mitad1[cont1]<=mitad2[cont2]
-			array_temp[cont_temp] = mitad1[cont1]
-				cont_temp += 1
-				cont1 += 1
-			else
-				array_temp[cont_temp] = mitad2[cont2]
-				cont_temp += 1
-				cont2 += 1
-			end
-		end
-		
-		while cont1 < mitad1.count
-			array_temp[cont_temp] = mitad1[cont1]
-			cont_temp += 1
-			cont1 += 1
-		end
-		
-		while cont2 < mitad2.count
-			array_temp[cont_temp] = mitad2[cont2]
-			cont_temp += 1
-			cont2 += 1
-		end
+    if cont1 < mitad1.count
+      array_temp = rellenar_array(array_temp, cont_temp, mitad1, cont1)
+    elsif cont2 < mitad2.count
+      array_temp = rellenar_array(array_temp, cont_temp, mitad2, cont2)
+    end
 
-    	return array_temp
-	end
+    array_temp
+  end
+
+  def rellenar_array(array_final, cont_final, array, cont)
+    while cont < array.count
+      array_final[cont_final] = array[cont]
+      cont_final += 1
+      cont += 1
+    end
+    array_final
+  end
 end
--- mergesort_test.rb
+++ mergesort_test.rb
@@ -1,28 +1,30 @@
 require_relative('mergesort')
 require 'test/unit'
-class Mergesort_test < Test::Unit::TestCase
+#
+class MergesortTest < Test::Unit::TestCase
+  def test_cero_elemento
+    assert_equal(Mergesort.new.merge_sort([]), [])
+  end
 
-	def test_cero_elemento
-		assert_equal(Mergesort.new.merge_sort([]), [])
-	end
+  def test_un_elemento
+    assert_equal(Mergesort.new.merge_sort([5]), [5])
+  end
 
-	def test_un_elemento
-		assert_equal(Mergesort.new.merge_sort([5]), [5])
-	end
+  def test_simple
+    assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5, 2]),
+                 [-1, 0, 2, 5, 6, 7])
+  end
 
-	def test_simple
-		assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5, 2]), [-1, 0, 2, 5, 6, 7])
-	end
+  def test_num_impar_elemementos
+    assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5]), [-1, 0, 5, 6, 7])
+  end
 
-	def test_num_impar_elemementos
-		assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5]), [-1, 0, 5, 6, 7])
-	end
+  def test_float
+    assert_equal(Mergesort.new.merge_sort([6.5, 7.3, -1.0, 0, 5.2, 2.1]),
+                 [-1.0, 0, 2.1, 5.2, 6.5, 7.3])
+  end
 
-	def test_float
-		assert_equal(Mergesort.new.merge_sort([6.5, 7.3, -1.0, 0, 5.2, 2.1]), [-1.0, 0, 2.1, 5.2, 6.5, 7.3])
-	end
-
-	def test_caracteres
-		assert_equal(Mergesort.new.merge_sort(["e", "b", "d", "z", "a", "s"]), ["a", "b", "d", "e", "s", "z"])
-	end
+  def test_caracteres
+    assert_equal(Mergesort.new.merge_sort(%w(e b d z a s)), %w(a b d e s z))
+  end
 end
