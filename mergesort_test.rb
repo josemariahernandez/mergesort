334511397be0aad69bd321de06a1bfe4028a1c34
Jose Maria <josemaria.hernandez@medianet.es>
2015-06-11 13:53:30 +0200

    Añadido test float

--- mergesort_test.rb
+++ mergesort_test.rb
@@ -14,7 +14,11 @@ class Mergesort_test < Test::Unit::TestCase
 		assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5, 2]), [-1, 0, 2, 5, 6, 7])
 	end
 
-	def test_impar
+	def test_num_impar_elemementos
 		assert_equal(Mergesort.new.merge_sort([6, 7, -1, 0, 5]), [-1, 0, 5, 6, 7])
+	end
+	
+	def test_float
+		assert_equal(Mergesort.new.merge_sort([6.5, 7.3, -1.0, 0, 5.2, 2.1]), [-1.0, 0, 2.1, 5.2, 6.5, 7.3])
 	end
 end
