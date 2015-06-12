36b301360969b417bac200d88fa52a2b71a77fce
Jose Maria <josemaria.hernandez@medianet.es>
2015-06-12 13:15:51 +0200

    Codigo refactorizado

--- mergesort.rb
+++ mergesort.rb
@@ -6,25 +6,31 @@ class Mergesort
     mitad_array = array.count / 2
     mitad1 = merge_sort(array.slice(0, mitad_array))
     mitad2 = merge_sort(array.slice(mitad_array, array.count - mitad_array))
-    array_temp = []
-    cont_temp = cont1 = cont2 = 0
 
-    while cont1 < mitad1.count && cont2 < mitad2.count
+    combinar_arrays(mitad1, mitad2)
+  end
+
+  def combinar_arrays(array1, array2)
+    array_temp = [], cont1 = cont2 = cont_temp = 0
+
+    while quedan_elementos(array1, cont1, array2, cont2)
       array_temp[cont_temp], cont1, cont2 =
-              asignar_valor_menor(mitad1[cont1], cont1, mitad2[cont2], cont2)
+              asignar_valor_menor(array1[cont1], cont1, array2[cont2], cont2)
       cont_temp += 1
     end
 
-    array_temp = rellenar_array(array_temp, cont_temp,
-                                mitad1, cont1) if cont1 < mitad1.count
+    return completar_array(array_temp, cont_temp,
+                           array1, cont1) if cont1 < array1.count
 
-    array_temp = rellenar_array(array_temp, cont_temp,
-                                mitad2, cont2) if cont2 < mitad2.count
-
-    array_temp
+    return completar_array(array_temp, cont_temp,
+                           array2, cont2) if cont2 < array2.count
   end
 
-  def rellenar_array(array_final, cont_final, array, cont)
+  def quedan_elementos(array1, cont1, array2, cont2)
+    cont1 < array1.count && cont2 < array2.count
+  end
+
+  def completar_array(array_final, cont_final, array, cont)
     while cont < array.count
       array_final[cont_final] = array[cont]
       cont_final += 1
